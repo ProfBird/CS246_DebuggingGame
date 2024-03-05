9f4ff9acc7ff31573a64778635d5851e17b5187d
Brian Bird <birdb@lanecc.edu>
2024-03-05 15:44:42 -0800

    Add project files.

---
+++ DebuggingGame/Bug.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingGame
+{
+    public interface Bug
+    {
+        public string Name { get; }
+        public string Move();
+
+        public bool Kill(string exterminator);
+
+    }
+
+    public class Cockroach : Bug
+    {
+        public string Name { get; } = "cockroach";
+        public string Move() {
+            return "Crawling";
+        } 
+
+        public bool Kill(string exterminator) {
+            return exterminator.ToLower() == "spray";
+        }
+    }
+
+    public class Fly : Bug
+    {
+        public string Name { get; } = "fly";
+        public string Move()
+        {
+            return "flying";
+        }
+
+        public bool Kill(string exterminator)
+        {
+            return exterminator.ToLower() == "swat";
+        }
+    }
+}
---
+++ DebuggingGame/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DebuggingGame;
+
+Console.WriteLine("Hello, World!");
+
+Random randGen = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    Bug bug = new Fly();  // just to make things happy
+    int bugSelect = randGen.Next(1, 3);
+    switch(bugSelect)
+    {
+        case 1:
+            bug = new Cockroach();
+            break;
+        case 2:
+            bug = new Fly();
+            break;
+    }
+
+    Console.WriteLine("The " + bug.Name + " is " + bug.Move());
+
+}
